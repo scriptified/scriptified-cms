2b30a02c1f4c8c22b11ab1e12204154e5a395ba5
Prateek Surana <prateeksurana3255@gmail.com>
2021-04-18 20:04:42 +0530

    :sparkles: Added dev of the week and gif types

---
+++ api/dev-of-the-week/controllers/dev-of-the-week.js
@@ -0,0 +1,51 @@
+"use strict";
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+const parseEntity = entity => ({
+    name: entity.name,
+    bio: entity.bio,
+    profileImg: entity.profileImg,
+    profileLink: {
+      website: entity.website || undefined,
+      twitter: entity.twitter || undefined,
+      github: entity.github || undefined,
+      youtube: entity.youtube || undefined,
+      linkedin: entity.linkedin || undefined,
+      instagram: entity.instagram || undefined,
+    },
+  });
+
+module.exports = {
+  async find(ctx) {
+    let entities;
+    console.log(strapi.services);
+    if (ctx.query._q) {
+      entities = await strapi.services['dev-of-the-week'].search(ctx.query);
+    } else {
+      entities = await strapi.services['dev-of-the-week'].find(ctx.query);
+    }
+
+    const sanitizedEntities = entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models['dev-of-the-week'] })
+    );
+
+    const parsedEntities = sanitizedEntities.map((entity) => parseEntity(entity));
+
+    return parsedEntities;
+  },
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services['dev-of-the-week'].findOne({ id });
+    const sanitizedEntity = sanitizeEntity(entity, { model: strapi.models['dev-of-the-week'] });
+
+    return parseEntity(sanitizedEntity);
+  },
+
+};
